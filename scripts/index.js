09ee4641230bcdc09adaa39883d4483b84fb00e9
Katerina Matsuleva <katerina.matsuleva@gmail.com>
2021-06-04 07:21:56 +0300

    feat: add responsive styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,31 @@
-let mainContainer = document.querySelector('.main'),
-    popupContainer = mainContainer.querySelector('.popup'),
-    profileContainer = mainContainer.querySelector('.profile'),
-
-    userName = profileContainer.querySelector(".profile-info__name"),
-    userPosition = profileContainer.querySelector(".profile-info__position"),
-
-    popupCloseButton = popupContainer.querySelector('.popup__close'),
-    profileEditButton = profileContainer.querySelector('.edit-button');
-
-    form = popupContainer.querySelector(".popup__fields"),
-    formNameField = popupContainer.querySelector(".popup__field_value_name"),
-    formPositionField = popupContainer.querySelector(".popup__field_value_position");
+let mainContainer = document.querySelector(".main"),
+  popupContainer = mainContainer.querySelector(".popup"),
+  profileContainer = mainContainer.querySelector(".profile"),
+  userName = profileContainer.querySelector(".profile-info__name"),
+  userPosition = profileContainer.querySelector(".profile-info__position"),
+  popupCloseButton = popupContainer.querySelector(".popup__close"),
+  profileEditButton = profileContainer.querySelector(".edit-button"),
+  form = popupContainer.querySelector(".popup__fields"),
+  formNameField = popupContainer.querySelector(".popup__field_value_name"),
+  formPositionField = popupContainer.querySelector(".popup__field_value_position");
 
 
-profileEditButton.addEventListener('click', function() {
-  popupContainer.classList.add('popup_opened');
+function popupOpen(container, selector) {
+  container.classList.add(selector);
+}
+
+function popupClose(container, selector) {
+  container.classList.remove(selector);
+}
+
+profileEditButton.addEventListener("click", function () {
+  popupOpen(popupContainer, "popup_opened");
 });
 
-popupCloseButton.addEventListener('click', function() {
-  popupContainer.classList.remove('popup_opened');
+popupCloseButton.addEventListener("click", function () {
+  popupClose(popupContainer, "popup_opened");
 });
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -27,7 +33,7 @@ function formSubmitHandler(evt) {
   userName.textContent = formNameField.value;
   userPosition.textContent = formPositionField.value;
 
-  popupContainer.classList.remove('popup_opened');
+  popupClose(popupContainer, "popup_opened");
 }
 
-form.addEventListener('submit', formSubmitHandler);
+form.addEventListener("submit", formSubmitHandler);
